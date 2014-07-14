80407a45e3fe7141958b27862b46b4029b3eb1dc
monopoint <espen@monopoint.net>
2014-07-14 08:24:33 +0200

    create and write to settings file

--- src/net/monopoint/parrot/Parrot.java
+++ src/net/monopoint/parrot/Parrot.java
@@ -13,21 +13,29 @@ public class Parrot {
 
     public Parrot() {
 
+        // Loading properties
         Properties properties = new Properties();
 
         try {
+            if (!settingsFileExists()){
+                createSettingsFile();
+                System.out.println("Could not find settings file, so I created it. You should edit it for your needs.");
+            }
+
             properties.load(getPropertiesUrl().openStream());
+
+
         } catch (IOException e) {
             System.out.println("Unable to load settings file.");
             e.printStackTrace();
         }  catch (NullPointerException e){
-            System.out.println("Could not find settings file, so I created it. You should edit it for your needs.");
-            createSettingsFile();
+            System.out.println("Unable to load settings file.");
+            e.printStackTrace();
         }
-
-
-
     }
+
+
+    // TODO: should return path as atring, not URL, as this includes "file:/"
 
     private URL getAppLocationPath(){
         URL url = Main.class.getProtectionDomain().getCodeSource().getLocation();
@@ -40,14 +48,27 @@ public class Parrot {
         return propertiesUrl;
     }
 
-    private void createSettingsFile(){
-        File settingsFile = new File("settings.properties");
+    private boolean settingsFileExists(){
+        URL path = getAppLocationPath();
+        File file = new File(path.toString() + settingsFileName);
+        return file.exists();
+    }
 
+    private void createSettingsFile(){
+        File settingsFile = new File(getAppLocationPath().toString()+settingsFileName);
 
         try {
-            Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(settingsFileName), "utf-8"));
-            writer.write("Something\n");
-            writer.write("other\n");
+            settingsFile.createNewFile();
+            Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(getAppLocationPath().toString()+settingsFileName), "utf-8"));
+            writer.write("# Settings for parrot.\n");
+            writer.write("\n");
+            writer.write("# files in this directory are ignored\n");
+            writer.write("# ignore_dir = \n");
+            writer.write("\n");
+            writer.write("\n");
+            writer.write("\n");
+            writer.write("\n");
+
             writer.close();
 
         } catch (UnsupportedEncodingException e) {
